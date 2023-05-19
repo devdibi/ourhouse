ac3d554870639c55b40a0ee1ab879f8eb35baadb
unknown <kjenotn@gmail.com>
2023-05-19 16:43:41 +0900

    router.js 파일 하나로 통합

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,18 +3,19 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/user">User</router-link>
     </nav>
     <router-view/>
   </div>
 </template>
 
 <script>
-import TheHeader from "@/components/TheHeader.vue";
+// import TheHeader from "@/components/TheHeader.vue";
 
 export default {
 	name: "App",
 	components: {
-		TheHeader,
+		// TheHeader,
 	},
 };
 </script>
