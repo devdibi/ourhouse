7c3ac095b2462fb30df0ab146369c4a033c5a5c3
김민수 <mskim.spring@gmail.com>
2023-05-19 09:32:39 +0900

    Header 추가

---
+++ frontend/src/components/HeaderView.vue
@@ -0,0 +1,54 @@
+<template>
+	<header>
+		<div>
+			<b-navbar toggleable="md" type="dark" class="purple">
+				<b-navbar-brand href="#" tag="h1">NavBar</b-navbar-brand>
+
+				<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+				<b-collapse id="nav-collapse" is-nav>
+					<b-navbar-nav class="ml-5">
+						<b-nav-item to="/land" link-classes="text-white">지도 검색</b-nav-item>
+						<b-nav-item link-classes="text-white">공지 사항</b-nav-item>
+						<b-nav-item>자유 게시판</b-nav-item>
+						<b-nav-item-dropdown text="내 정보">
+							<b-dropdown-group id="dropdown-group-1" header="부동산">
+								<b-dropdown-item href="#">관심 단지</b-dropdown-item>
+								<b-dropdown-item href="#">관심 매물</b-dropdown-item>
+							</b-dropdown-group>
+							<b-dropdown-divider></b-dropdown-divider>
+							<b-dropdown-item href="#">마이 페이지</b-dropdown-item>
+						</b-nav-item-dropdown>
+					</b-navbar-nav>
+
+					<!-- Right aligned nav items -->
+					<b-navbar-nav class="ml-auto">
+						<b-nav-item href="#">로그인</b-nav-item>
+						<b-nav-item href="#">회원 가입</b-nav-item>
+					</b-navbar-nav>
+				</b-collapse>
+			</b-navbar>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "HeaderView",
+	components: {},
+	data() {
+		return {
+			message: "",
+		};
+	},
+	created() {},
+	methods: {},
+};
+</script>
+
+<style scoped>
+.purple {
+	background: rgba(67, 56, 202, 0.8);
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,17 @@ import App from "./App.vue";
 import router from "./router/router";
 import store from "./store/store";
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
 Vue.config.productionTip = false;
 
 new Vue({
