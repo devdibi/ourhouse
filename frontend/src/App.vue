b509e5c8512563bbcd5e7589bd0e9cdb10e9e233
seunghyeon <kshv02@gmail.com>
2023-05-20 18:57:09 +0900

    Header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div id="app">
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <router-link to="/user">User</router-link>
-    </nav>
-    <router-view/>
+    </nav> -->
+    <the-header></the-header>
+    <router-view />
   </div>
 </template>
 
 <script>
-// import TheHeader from "@/components/TheHeader.vue";
+import TheHeader from "@/components/TheHeader.vue";
 
 export default {
-	name: "App",
-	components: {
-		// TheHeader,
-	},
+  name: "App",
+  components: {
+    TheHeader,
+  },
 };
 </script>
