eb1c28afdc8900e05818ec6ef32cbdc7925f03cf
김정은 <kjenotn@gmail.com>
2023-05-21 16:28:04 +0900

    회원가입 관련 컨트롤러 수정

--- backend/src/main/java/com/ssafy/ourhouse/controller/UserController.java
+++ backend/src/main/java/com/ssafy/ourhouse/controller/UserController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +29,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 //추후 httpStatus 수정
-
+@CrossOrigin(origins = { "*" }, maxAge = 600)
 @RestController
 @RequestMapping("/user")
 @Api(value="USER 컨트롤러 API")
