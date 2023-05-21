72d79ac034268f75448ccb7246d033bca996bcef
김민수 <mskim.spring@gmail.com>
2023-05-21 11:22:32 +0900

    add user domain

---
+++ backend/src/main/java/com/ssafy/ourhouse/domain/User.java
@@ -0,0 +1,62 @@
+package com.ssafy.ourhouse.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.*;
+
+@Setter
+@Getter
+public class User implements UserDetails {
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private int gender;
+
+    private int ageGroup;
+
+    private int dwellArea;
+
+    private int favoriteArea;
+
+    private int status;
+
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
