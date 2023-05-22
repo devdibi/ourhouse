0f01a4f363f53e51e26084550ea3c12a1a725ac4
김민수 <mskim.spring@gmail.com>
2023-05-22 10:34:41 +0900

    Cors 설정

--- backend/src/main/java/com/ssafy/ourhouse/config/SecurityConfiguration.java
+++ backend/src/main/java/com/ssafy/ourhouse/config/SecurityConfiguration.java
@@ -4,8 +4,15 @@ import com.ssafy.ourhouse.domain.Role;
 import com.ssafy.ourhouse.filter.JwtAuthenticationFilter;
 import com.ssafy.ourhouse.service.UserService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -19,6 +26,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -26,41 +36,42 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
+	private final JwtAuthenticationFilter jwtAuthFilter;
+	private final AuthenticationProvider authenticationProvider;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf()
-                    .disable()
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+		http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authenticationProvider(authenticationProvider)
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 
-                .authorizeHttpRequests()
-                .antMatchers(
-                        // 토큰 발급 URL
-                        "/user/**",
-                        // Swagger 허용 URL
-                        "/v2/api-docs",
-                        "/v3/api-docs",
-                        "/v3/api-docs/**",
-                        "/swagger-resources",
-                        "/swagger-resources/**",
-                        "/configuration/ui",
-                        "/configuration/security",
-                        "/swagger-ui/**",
-                        "/webjars/**",
-                        "/swagger-ui.html"
-                ).permitAll()
+				.authorizeHttpRequests().antMatchers(
+						// 토큰 발급 URL
+						"/user/**",
+						// House
+						"/house/sido", "/house/sigungu", "/house/dong", "/house/",
+						// Swagger 허용 URL
+						"/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
+						"/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
+						"/webjars/**", "/swagger-ui.html")
+				.permitAll()
 //                .antMatchers("").hasRole(Role.USER.name())
-                .anyRequest()
-                .authenticated();
+				.anyRequest().authenticated();
 
-        return http.build();
+		return http.build();
+	}
+
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+//        configuration.setExposedHeaders(List.of("x-auth-token"));
+//        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- backend/src/main/java/com/ssafy/ourhouse/config/WebMvcConfiguration.java
+++ backend/src/main/java/com/ssafy/ourhouse/config/WebMvcConfiguration.java
@@ -12,15 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@MapperScan(basePackages = { "com.ssafy.ourhouse.mapper" })
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-
-		registry.addMapping("/**").allowedOrigins("*")
-				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-						HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
-						HttpMethod.PATCH.name())
-				.maxAge(1800);
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//
+//		registry.addMapping("/**").allowedOrigins("*")
+//				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+//						HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
+//						HttpMethod.PATCH.name())
+//				.allowCredentials(false).maxAge(1800);
+//	}
 
 //	Swagger UI 실행시 404처리
 //	Swagger2 일경우
