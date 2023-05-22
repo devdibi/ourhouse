07f08b69314e6740b2c7f0e3880036ca1a73511a
unknown <kjenotn@gmail.com>
2023-05-22 10:15:55 +0900

    정보 수집 동의 스타일 수정 및 버튼 추가

--- frontend/src/components/user/UserJoinAgreement.vue
+++ frontend/src/components/user/UserJoinAgreement.vue
@@ -2,33 +2,42 @@
   <div id="box">
     <div>
       <div>
-        <div class="title">
-          개인정보 수집 이용 동의 <span class="sub">필수</span>
+        <div class="title" id="title1">
+          개인정보 수집 이용 동의 <span class="sub">(필수)</span>
         </div>
         <div>
-          <b-form-textarea id="textarea1" v-model="text1" rows="3" max-rows="6"></b-form-textarea>
-          <pre class="mt-3 mb-0">{{ text1 }}</pre>
+          <b-form-textarea id="textarea1" class="textarea" v-model="text1" rows="3" max-rows="6"
+            readonly></b-form-textarea>
         </div>
-        <b-form-checkbox id="checkbox1" v-model="status1" value="동의함" unchecked-value="동의하지 않음">
-          {{ status1 }}
-        </b-form-checkbox>
+        <div id="checkbox1">
+          <b-form-checkbox v-model="status1" value="동의함" unchecked-value="동의하지 않음">
+            {{ status1 }}
+          </b-form-checkbox>
+        </div>
       </div>
       <div>
-        <div class="title">
-          마케팅 정보 수신 동의 <span class="sub">선택</span>
+        <div class="title" id="title2">
+          마케팅 정보 수신 동의 <span class="sub">(선택)</span>
         </div>
         <div>
-          <b-form-textarea id="textarea2" v-model="text2" rows="3" max-rows="6"></b-form-textarea>
-          <pre class="mt-3 mb-0">{{ text2 }}</pre>
+          <b-form-textarea id="textarea2" class="textarea" v-model="text2" rows="3" max-rows="6"
+            readonly></b-form-textarea>
         </div>
-        <b-form-checkbox id="checkbox2" v-model="status2" value="동의함" unchecked-value="동의하지 않음">
-          {{ status2 }}
-        </b-form-checkbox>
-        <b-form-checkbox id="checkbox3" v-model="status3" value="동의함" unchecked-value="동의하지 않음" @change="checkAll()">
-          {{ status3 }}
-        </b-form-checkbox>
-        <input type="button" value="이전" class="button" @click="previous">
-        <input type="button" value="다음" class="button" @click="next">
+        <div id="checkbox2">
+          <b-form-checkbox v-model="status2" value="동의함" unchecked-value="동의하지 않음">
+            {{ status2 }}
+          </b-form-checkbox>
+        </div>
+        <div id="checkbox3">
+          개인정보 수집 및 이용, 마케팅 정보 수신(선택)에 모두 동의합니다.
+        </div>
+        <div id="checkbox4">
+          <b-form-checkbox v-model="status3" value="동의함" unchecked-value="동의하지 않음" @change="checkAll()">
+            {{ status3 }}
+          </b-form-checkbox>
+        </div>
+        <input type="button" id="previousbtn" value="이전" class="button" @click="previous">
+        <input type="button" id="nextbtn" value="다음" class="button" @click="next">
       </div>
     </div>
   </div>
@@ -72,11 +81,9 @@ export default {
 
 <style scoped>
 * {
-  font-family: "Noto Sans";
+  left: 35px;
+  font-family: 'Noto Sans';
   font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 22px;
 }
 
 #box {
@@ -95,22 +102,120 @@ export default {
   border-radius: 16px;
 }
 
+.title {
+  position: absolute;
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 33px;
+  color: #000000;
+}
+
+#title1 {
+  top: 32px;
+}
+
+#title2 {
+  top: 240px;
+}
+
+.sub {
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25px;
+}
+
+.textarea {
+  position: absolute;
+  width: 730px;
+  height: 100px;
+  background: #ffffff;
+}
+
+#textarea1 {
+  top: 78px;
+}
+
+#textarea2 {
+  top: 285px;
+}
+
+#checkbox1 {
+  position: absolute;
+  top: 173px;
+  left: 630px;
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #000000;
+}
+
+#checkbox2 {
+  position: absolute;
+  padding: 0;
+  top: 380px;
+  left: 630px;
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #000000;
+}
+
+#checkbox3 {
+  position: absolute;
+  top: 420px;
+  left: 410px;
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #000000;
+}
+
+#checkbox4 {
+  position: absolute;
+  top: 440px;
+  left: 630px;
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #000000;
+}
+
+#previousbtn{
+  left:35px;
+}
+#nextbtn{
+  left:415px;
+}
+
 .button {
+  position: absolute;
   border: 0;
-  /* position: absolute; */
   width: 350px;
   height: 60px;
-  /* left: 975px;
-  top: 714px; */
   background: #e8e8e8;
   border-radius: 5px;
+  top: 490px;
 
   font-family: "Noto Sans";
   font-style: normal;
-  font-weight: 400;
   font-size: 16px;
   line-height: 22px;
   text-align: center;
   color: #989898;
+}
+
+.button:hover{
+  background: #6960d5;
+  color: #ffffff;
 }
 </style>
