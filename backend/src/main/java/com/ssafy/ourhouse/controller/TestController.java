16dac29ce94735c1c6812bc1d41b949ef9c98c9f
김민수 <mskim.spring@gmail.com>
2023-06-30 17:46:13 +0900

    .

---
+++ backend/src/main/java/com/ssafy/ourhouse/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.ssafy.ourhouse.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/one")
+    public void test() {
+        log.info("enter");
+    }
+}
